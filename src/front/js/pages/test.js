14c5244f582409da2b2caa1f894422323974a07d
Bruno Marques <besmarques@gmail.com>
2022-03-09 22:23:16 +0000

    testes 2

--- src/front/js/pages/test.js
+++ src/front/js/pages/test.js
@@ -1,36 +1,46 @@
 import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
 
-let country = "FR";
-let city = "Paris";
-let base = "";
-let location = ["Eiffel Tower", "Triumph Arc", "Trocadero"];
+function getFromMapBox () {
 
-let resultObject = [];
-
-let fetchURL = ""
-
-let mapBoxToken = "pk.eyJ1IjoiYmVzbWFycXVlcyIsImEiOiJja3p2cGRucDQwMGliMm9rNnpuOG90MG9nIn0.5n3XuDKIqcxsIDs-1VGs7g";
-
-//fetchURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + location + ".json?country=" + country + "&city=" + city + "&access_token=" + mapBoxToken;
-
-for(let i = 0; i < location.length; i++){
-    fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/" + location[i] + ".json?country=" + country + "&city=" + city + "&access_token=" + mapBoxToken , requestOptions)
-.then(response => response.text())
-.then(result => console.log(result))
-.catch(error => console.log('error', error));
 }
 
-
 const FetchTest = () => {
+    
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+    
+    let country = "FR";
+    let city = "Paris";
+    let base = "";
+    let location = ["Eiffel Tower", "Triumph Arc", "Trocadero"];
+    
+   
+
+    let resultObject = [];
+    
+    let fetchURL = ""
+    
+    let mapBoxToken = "pk.eyJ1IjoiYmVzbWFycXVlcyIsImEiOiJja3p2cGRucDQwMGliMm9rNnpuOG90MG9nIn0.5n3XuDKIqcxsIDs-1VGs7g";
+    
+    //fetchURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + location + ".json?country=" + country + "&city=" + city + "&access_token=" + mapBoxToken;
+    
+    for(let i = 0; i < location.length; i++){
+        fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/" + location[i] + ".json?country=" + country + "&city=" + city + "&access_token=" + mapBoxToken , requestOptions)
+    .then(response => response.json())
+    .then(result => {resultObject.push(result); console.log(resultObject.length)} )
+    .catch(error => console.log('error', error));
+    }
+    
+    console.log(resultObject.length)
+
     return(
         <>
-            <h1>this is a test</h1>
+            <h1>test</h1>
+            <p>{resultObject[0]}</p>
         </>
     )
 }
